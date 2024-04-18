ef64ccd451cc2e684952fa8b7a8da1b0c888320e
jandje <jeroenvenderbosch@hotmail.com>
2024-04-18 19:52:35 +0200

    Delete ZeepkistExchange/obj directory

--- ZeepkistExchange/obj/Debug/net472/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ZeepkistExchange
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "ZeepkistExchange";
-        public const string PLUGIN_NAME = "ZeepkistExchange";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
